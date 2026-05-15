b550f463673171df1ccf2f2c142ac50d6e12fb26
Srushti Lakare <srushti1924@gmail.com>
2026-05-15 17:54:54 +0530

    DATABASE SETUP + USER MODEL

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from app.database import engine
+import app.models as models
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
 
 @app.get("/")
 def home():
